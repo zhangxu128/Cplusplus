e39dde19b25a2dc81fb45e385fc7ee0c8e37ae36
zhangxu128 <731172278@qq.com>
2019-02-12 20:14:54 +0800

    Java练习

---
+++ CY_system/src/cy_jiemian/ZhuJieMian.java
@@ -0,0 +1,19 @@
+package cy_jiemian;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ZhuJieMian extends JWindow{
+
+	JProgressBar jpb;
+	JLabel j11;
+	int width,height;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ZhuJieMian zhujiemian=new ZhuJieMian();
+	}
+    public ZhuJieMian(){
+    	j11=new JLabel(new ImageIcon());
+    }
+}
