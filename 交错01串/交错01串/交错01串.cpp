d137aec2b0d347c405cc78f76c9035e6f52cbbd2
zhangxu128 <731172278@qq.com>
2019-04-30 23:13:17 +0800

    交错01串

---
+++ 交错01串/交错01串/交错01串.cpp
@@ -0,0 +1,39 @@
+//题目描述
+//如果一个01串任意两个相邻位置的字符都是不一样的, 我们就叫这个01串为交错01串。例如: "1", "10101", "0101010"都是交错01串。
+//小易现在有一个01串s, 小易想找出一个最长的连续子串, 并且这个子串是一个交错01串。小易需要你帮帮忙求出最长的这样的子串的长度是多少。
+//输入描述 :
+//输入包括字符串s, s的长度length(1 ≤ length ≤ 50), 字符串中只包含'0'和'1'
+//输出描述 :
+//	输出一个整数, 表示最长的满足要求的子串长度。
+//	示例1
+//	输入
+//	111101111
+//	输出
+//	3
+
+
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+	int res = 0;
+	for (int i = 1; i < str.size();) {
+		int k = i - 1;
+		while (i < str.size()) {
+			if (str[i] != str[i - 1])
+				i++;
+			else
+				break;
+		}
+		if (res < i - k)
+			res = i - k;
+		i++;
+	}
+	cout << res << endl;
+	system("pause");
+	return 0;
+}
