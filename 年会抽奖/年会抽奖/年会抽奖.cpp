ad823e5c4fb30fdf28ea3b4af43498f36e19bb3a
zhangxu128 <731172278@qq.com>
2019-07-11 21:59:20 +0800

    年会抽奖

---
+++ 年会抽奖/年会抽奖/年会抽奖.cpp
@@ -0,0 +1,38 @@
+//今年公司年会的奖品特别给力，但获奖的规矩却很奇葩：
+//1. 首先，所有人员都将一张写有自己名字的字条放入抽奖箱中；
+//2. 待所有字条加入完毕，每人从箱中取一个字条；
+//3. 如果抽到的字条上写的就是自己的名字，那么“恭喜你，中奖了！”
+//现在告诉你参加晚会的人数，请你计算有多少概率会出现无人获奖？
+//输入描述 :
+//输入包含多组数据，每组数据包含一个正整数n（2≤n≤20）。
+//输出描述 :
+//对应每一组数据，以“xx.xx%”的格式输出发生无人获奖的概率。
+//示例1
+//输入
+//2
+//输出
+//50.00%
+
+
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	long long data[21] = { 1, 0, 1 }, fac = 2;
+	double rlt[21] = { 0.00, 0.00, 50.00 };
+	for (int i = 3; i <= 20; i++) {
+		data[i] = (i - 1)*(data[i - 1] + data[i - 2]);
+		fac *= i;
+		rlt[i] = (double)data[i] / fac * 100;
+	}
+	int n;
+	while (~scanf("%d", &n)) {
+		//printf("%0.2llf%%\n", rlt[n]);
+		cout << fixed << setprecision(2) << rlt[n] << "%" << endl;
+	}
+	return 0;
+}
